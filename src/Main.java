926960da284097da0ff4d8a0f9e28fe5c5d1d127
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-08-31 19:39:12 +0900

    src/Main.javaを追加

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
-        List<String> result = new ArrayList<>();
+        List<String> result = names.stream().filter(name -> name.contains("a")).toList();
 
-        for (String name : names){
-            if (name.contains("a")){
-                result.add(name);
-            }
-        }
         System.out.println(result);
     }
 }
