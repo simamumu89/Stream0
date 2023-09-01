58c89c77278989028e68b693959cc667a71a60ce
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-01 21:05:06 +0900

    src/Main.javaを追加

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
-        List<String> result = names.stream().filter(name -> name.contains("a")).toList();
-
-        System.out.println(result);
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
     }
 }
