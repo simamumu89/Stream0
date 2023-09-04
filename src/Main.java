eadab6ab9c71b7027d920c709d239b4230ae83d2
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-04 11:55:31 +0900

    namelistにshimamuraを追加

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> namelist = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
+        List<String> namelist = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida", "shimamura");
 
         namelist.forEach(System.out::println);
     }
